7bb20c8d60b79921bb5fb76ae5c5e7a58ca89e13
mihalin <stopa3.10.1996@mail.ru>
2020-11-26 01:21:50 +0300

    Analysis of one video (issue #6)

--- gui/gui.py
+++ gui/gui.py
@@ -9,7 +9,7 @@ from settings import Settings
 
 
 class Gui:
-    interval = 0.3  # Период обновления окна в секундах
+    interval = 0.1  # Период обновления окна в секундах
 
     def __init__(self, run_callback: Callable, close_callback: Callable):
         self._root = tk.Tk()
@@ -30,6 +30,13 @@ class Gui:
         tk.Entry(master=self._root, textvariable=self._api, width=40).pack()
         self._api.set(Settings.api_key())
 
+        label = tk.Label(width=40)
+        label["text"] = "ID Видео (для одного видео):"
+        label.pack()
+
+        self._video = tk.StringVar()
+        tk.Entry(master=self._root, textvariable=self._video, width=40).pack()
+
         self._start_btn = tk.Button(
             master=self._root, text="Начать", command=lambda: run_callback(self)
         )
@@ -47,6 +54,10 @@ class Gui:
     @property
     def api(self) -> str:
         return self._api.get()
+
+    @property
+    def video(self):
+        return self._video.get()
 
     async def run(self):
         # По образцу https://gist.github.com/Lucretiel/e7d9a50b7b1960a56a1c
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import logging
 from datetime import date, datetime
 from itertools import chain
 from statistics import export_statistics, get_statistics
-from typing import Iterable
+from typing import Iterable, Optional
 
 from aiohttp import ClientSession
 
@@ -12,17 +12,14 @@ from gui import Gui
 from youtube import ChannelVideo, Comment, YouTubeApi
 
 
-async def main(api_key: str, video_date: date):
+async def main(api_key: str, video_date: date, video: Optional[str] = None):
     """
     Запустить алгоритм выгрузки и анализа
     :param api_key: google API ключ
     :param video_date: отсечка по дате публикации видео
+    :param video: идентификатор видео (если нужно проанализировать всего лишь одно видео)
     :return:
     """
-    # Взять список каналов для анализа
-    channels = AntiIraApi.get_channels_list()
-    video_datetime = datetime(video_date.year, video_date.month, video_date.day)
-
     async with ClientSession() as session:
         # Взять список ботов
         bot_list_fetcher = AntiIraApi(session)
@@ -30,16 +27,30 @@ async def main(api_key: str, video_date: date):
 
         youtube_api = YouTubeApi(api_key, session)
 
-        # Скачать идентификаторы видео для каждого канала
-        tasks = [
-            youtube_api.list_videos(channel, video_datetime) for channel in channels
-        ]
-        videos_groups = await asyncio.gather(*tasks)
-        for channel, videos_in_channel in zip(channels, videos_groups):
-            logging.info(
-                "Channel " + channel + " has " + str(len(videos_in_channel)) + " videos"
-            )
-        videos: Iterable[ChannelVideo] = chain(*videos_groups)
+        if video:
+            # В интерфейсе указали конкретное видео - нужно анализировать его
+            videos = [ChannelVideo("none", video)]
+            logging.info("Use single video " + video)
+        else:
+            logging.info("Use channels.txt list")
+            # Взять список каналов для анализа
+            channels = AntiIraApi.get_channels_list()
+            video_datetime = datetime(video_date.year, video_date.month, video_date.day)
+
+            # Скачать идентификаторы видео для каждого канала
+            tasks = [
+                youtube_api.list_videos(channel, video_datetime) for channel in channels
+            ]
+            videos_groups = await asyncio.gather(*tasks)
+            for channel, videos_in_channel in zip(channels, videos_groups):
+                logging.info(
+                    "Channel "
+                    + channel
+                    + " has "
+                    + str(len(videos_in_channel))
+                    + " videos"
+                )
+            videos: Iterable[ChannelVideo] = chain(*videos_groups)
 
         # Теперь найти комментарии под каждым видео
         tasks = [
@@ -67,7 +78,7 @@ def run_callback(window: Gui):
     loop = asyncio.get_event_loop()
 
     # Добавить задачу на выгрузку данных
-    loop.create_task(main(window.api, window.date))
+    loop.create_task(main(window.api, window.date, window.video))
 
 
 def close_callback():
