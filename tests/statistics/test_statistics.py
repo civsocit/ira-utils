d3d628635dd06e86bafd483fceade427fecb1991
Andrey Aleksandrov <aleksandrov@hey.com>
2020-12-01 23:51:17 +0300

    test that stats ignore channel comments on its own videos

--- tests/statistics/test_statistics.py
+++ tests/statistics/test_statistics.py
@@ -9,7 +9,8 @@ from youtube import Comment
 comments = [
     Comment('UC5DqQh9__HKLd_HpDAXxsVw', 'verpkNic3SM', 'UCv3WZQIAXeprUopgMDWLvmQ', 'comment 1_1', 'UgxOzzAuLUP__6ye9xx4AaABAg'),
     Comment('UC5DqQh9__HKLd_HpDAXxsVw', 'verpkNic3SM', 'UCv3WZQIAXeprUopgMDWLvmQ', 'comment 1_2', 'Ugye-3kXWc2wVMImx-R4AaABAg'),
-    Comment('UC5DqQh9__HKLd_HpDAXxsVw', 'verpkNic3SM', 'UCSTJ4D8krCXQLq3_-V9ZYWg', 'comment 2_1', 'UgxxCXicAggdvs42ted4AaABAg')
+    Comment('UC5DqQh9__HKLd_HpDAXxsVw', 'verpkNic3SM', 'UCSTJ4D8krCXQLq3_-V9ZYWg', 'comment 2_1', 'UgxxCXicAggdvs42ted4AaABAg'),
+    Comment('UC5DqQh9__HKLd_HpDAXxsVw', 'verpkNic3SM', 'UC5DqQh9__HKLd_HpDAXxsVw', 'comment 3_1', 'UgxxCXicAggdvs42ted4AaABAg')
 ]
 
 def test_comments_counter():
@@ -17,6 +18,11 @@ def test_comments_counter():
 
     assert stats['UCv3WZQIAXeprUopgMDWLvmQ']['UC5DqQh9__HKLd_HpDAXxsVw'] == 2
     assert stats['UCSTJ4D8krCXQLq3_-V9ZYWg']['UC5DqQh9__HKLd_HpDAXxsVw'] == 1
+
+def test_ignore_channel_comments_on_itself():
+    stats = get_statistics(comments, [])
+    
+    assert 'UC5DqQh9__HKLd_HpDAXxsVw' not in stats
 
 def test_igore_comments_of_secified_users():
     stats = get_statistics(comments, ['UCv3WZQIAXeprUopgMDWLvmQ'])
