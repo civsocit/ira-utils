fbfab7c14cb66d42dac75cb51592ed4c5435c50f
Alexey Vaganov <a.vaganov@cm.expert>
2021-04-26 02:21:40 +0300

    feat(t30p.ru): argument parser with defaults

--- utils/t30p/README.md
+++ utils/t30p/README.md
@@ -2,8 +2,9 @@ t30p
 ====
 
 Файл user_id_list.txt содержит список userID, каждый userID в новой строке.
+В файле  report.tsv будет результат.
 
 ```shell script
 $ pip3 install -r ./requirements.txt
-$ python3 t30p.py < user_id_list.txt > report.tsv
+$ python3 t30p.py
 ```
--- utils/t30p/t30p.py
+++ utils/t30p/t30p.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
+import argparse
 import asyncio
 import csv
-import fileinput
+import io
 import re
-import sys
 from collections import defaultdict
 from typing import Tuple
 
@@ -31,21 +31,25 @@ async def fetch(client: ClientSession, user_id: str, pos: int = 0) -> Tuple[str,
             return user_id, page
 
 
-async def main() -> None:
+async def main(infile: io.TextIOWrapper, outfile: io.TextIOWrapper) -> None:
     result = defaultdict(int)
     async with ClientSession() as client:
         awaitables = []
-        for line in fileinput.input():
+        for line in infile:
             awaitables.append(fetch(client, user_id=line.strip()))
         fetch_results = await asyncio.gather(*awaitables)
-        for user_id, html in fetch_results:
-            for m in RE_LINK.finditer(html):
-                result[user_id,m.group(1)] += 1
-    writer = csv.writer(sys.stdout, csv.excel_tab)
+    for user_id, html in fetch_results:
+        for m in RE_LINK.finditer(html):
+            result[user_id, m.group(1)] += 1
+    writer = csv.writer(outfile, csv.excel_tab)
     writer.writerow(['userID', '', 'videoID', 'counter'])
     for key, val in result.items():
         writer.writerow([key[0], '', key[1], val])
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    parser = argparse.ArgumentParser()
+    parser.add_argument('infile', nargs='?', type=argparse.FileType('r'), default='user_id_list.txt')
+    parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default='report.tsv')
+    args = parser.parse_args()
+    asyncio.run(main(args.infile, args.outfile))
